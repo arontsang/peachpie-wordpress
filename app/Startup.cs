8035782e7a95dcc3cca7cefa91d3e806db075e31
Aron Tsang <arontsang@gmail.com>
2023-01-24 14:54:55 +0800

    Refactor

---
+++ app/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PeachPied.Demo;
+
+public class Startup
+{
+	private IConfiguration Configuration { get; }
+
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddMvc();
+		services.AddResponseCompression();
+		services.AddWordPress(options =>
+		{
+			// options.SiteUrl =
+			options.HomeUrl = Configuration["HomeUrl"] ?? "http://localhost:5004";
+                
+			// options.PluginContainer.Add<DashboardPlugin>(); // add plugin using dependency injection
+		});
+	}
+
+	public void Configure(IApplicationBuilder app, IHostEnvironment env, IConfiguration configuration)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+
+		app.UseWordPress();
+
+		app.UseDefaultFiles();
+	}
+}
